c432844d3b88389d15ae9bcd4d3658c56849cbd1
Zliplier <ktiranakul@gmail.com>
2025-08-10 12:58:17 +0700

    CV

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
-
+// let http = 'http';
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type' : 'text/html'});
+//     res.end('Hello World');
+// }).listen(9000);
 
 const showGreeting = (name) => {
     alert(`Welcome, ${name}!`);
@@ -7,14 +10,16 @@ const showGreeting = (name) => {
 
 const handleFormSubmit = (event) => {
     event.preventDefault(); // Prevent default page reload
-    const nameInput = document.getElementById("playerName");
-    const playerName = nameInput.value.trim();
-    if (playerName === "") {
-        alert("Please enter your name.");
-        return;
-    }
-    showGreeting(playerName);
+    // const nameInput = document.getElementById("playerName");
+    // const playerName = nameInput.value.trim();
+    // if (playerName === "") {
+    //     alert("Please enter your name.");
+    //     return;
+    // }
+    // showGreeting(playerName);
+    // window.location.href = "https://youtu.be/dQw4w9WgXcQ";
+    window.open("https://youtu.be/dQw4w9WgXcQ", '_blank').focus();
 };
 
-const form = document.getElementById("player-form");
-form.addEventListener("submit", handleFormSubmit);
+const form = document.getElementById("btn-contact");
+form.addEventListener("click", handleFormSubmit);
